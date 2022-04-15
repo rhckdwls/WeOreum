c295965b8706b57896ed8bbdba584baa63302403
rhckdwls <rhckdwls12@naver.com>
2022-04-15 12:31:21 +0900

    5

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -88,7 +88,7 @@ const config: Configuration = {
     static: { directory: path.resolve(__dirname) },
     proxy: {
       '/api/': {
-        target: 'http://localhost:3095',
+        target: 'http://localhost:3088',
         changeOrigin: true,
         ws: true,
       },
